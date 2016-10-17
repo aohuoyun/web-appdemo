aa23978032e141b87308393a318da8fc114816a6
aohuoyun <1034482804@qq.com>
2016-10-17 21:16:13 +0800

    update

---
+++ appdemo/src/components/Hello.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="page-swipe">
+    <mt-swipe :auto="4000">
+      <mt-swipe-item class="slide1">jhasdjhajshas</mt-swipe-item>
+      <mt-swipe-item class="slide2">2</mt-swipe-item>
+      <mt-swipe-item class="slide3">3</mt-swipe-item>
+    </mt-swipe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .mint-swipe {
+    height: 400px;
+    color: #fff;
+    font-size: 30px;
+    text-align: center;
+    margin-bottom: 20px;
+  }
+  .mint-swipe-item {
+    line-height: 400px;
+  }
+  .slide1 {
+    background-color: #0089dc;
+    color: #fff;
+  }
+  .slide2 {
+    background-color: #ffd705;
+    color: #000;
+  }
+  .slide3 {
+    background-color: #ff2d4b;
+    color: #fff;
+  }
+</style>
