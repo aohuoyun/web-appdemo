689bf4e263688582b7353481faf02646190b2161
aohuoyun <1034482804@qq.com>
2016-11-09 20:35:26 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
 import Vue from 'vue'
+import Mint from 'mint-ui';
+import VueHttp from 'vue-resource'
+import VueRouter from 'vue-router'
+import RouterConfig from "../config/RouterConfig" 
+import ApiConfig from "../config/api-config"
 import App from './App'
-
+import 'mint-ui/lib/style.css';
+Vue.use(Mint);
+Vue.use(VueRouter);
+Vue.use(VueHttp);
+var router = new VueRouter({
+    linkActiveClass: "active",
+    routes : RouterConfig
+})
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-})
+  router,
+  render: h => h(App)
+}).$mount('#app')
+window.router=router;
+Vue.prototype.extendApi=ApiConfig
